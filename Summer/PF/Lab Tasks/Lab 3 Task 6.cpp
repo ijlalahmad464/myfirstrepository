814933d2b0e8aea8fad148e5ffba7cb28e5114f7
ijlalahmad464 <ijlalahmad552@gmail.com>
2026-09-18 22:22:01 +0500

    Delete Summer directory

--- Summer/PF/Lab Tasks/Lab 3 Task 6.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int square;
-	for(int i=1; i<=10; i++){
-		square=i*i;
-		cout<<"The square of "<<i<<" is:"<<square<<endl;
-	}
-}
--- Summer/PF/Lab Tasks/Lab 3 Task5.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	for(int z=1; z<=5; z++){
-		cout<<z*z-1<<endl;
-	}
-}
